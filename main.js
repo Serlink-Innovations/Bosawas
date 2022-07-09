954916db696354708a188432ffe4d3ec3ae0565b
Kaleb Carrington <serlink.business@outlook.com>
2022-07-09 14:15:02 -0400

    v3.3

--- main.js
+++ main.js
@@ -2,3 +2,16 @@
 setTimeout(() => {
     document.getElementById("loading").style.display = "none"
 }, 2000);
+
+//Page Back to Top
+const backToTop = document.getElementById('back-to-top');
+
+function backToTopBtn() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+}
+backToTop.addEventListener('click', backToTopBtn);
