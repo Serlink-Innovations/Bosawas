83c6ce9998b89407f7027d6230d1f307130d0727
Serlink-Innovations <serlink.business@outlook.com>
2022-07-13 23:47:33 -0400

    v1.2.6

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ setTimeout(() => {
     document.getElementById("loading").style.display = "none"
 }, 2000);
 
+//Mobile menu function
+const toggle = document.getElementById('navToggler');
+const nav = document.getElementsByClassName('navbar')[0];
+
+function mobileSwitch() {
+
+    if (nav.classList.contains('navbar')) {
+        nav.classList.toggle('active');
+    }
+}
+toggle.addEventListener('click', mobileSwitch);
+
 //Page Back to Top
 const backToTop = document.getElementById('back-to-top');
 
